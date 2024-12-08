feec671e5b4059d194ca1ba2be99b1088913c6ee
Tej Magar <tejmagar20510@gmail.com>
2024-12-08 16:35:02 +0545

    add gif data

--- clip_extractor.py
+++ clip_extractor.py
@@ -1,6 +1,8 @@
 import subprocess
 import threading
 
+START_INDEX_FROM = 4
+
 
 def execute_ffmpeg(command, output_file):
     subprocess.run(command, check=True)
@@ -16,6 +18,9 @@ def extract_video_segments_as_gif(input_file, output_folder, segments):
 
     try:
         for idx, (start, end, emote_name) in enumerate(segments):
+            if idx < START_INDEX_FROM - 1:
+                continue
+
             output_file = f"{output_folder}/{emote_name}.gif"
             print(f"Processing segment {idx + 1}: {start}s to {end}s")
 
@@ -23,11 +28,11 @@ def extract_video_segments_as_gif(input_file, output_folder, segments):
             command = [
                 "ffmpeg",
                 "-ss", str(start),  # Start time
-                "-to", str(end),    # End time
-                "-i", input_file,   # Input file,
+                "-to", str(end),  # End time
+                "-i", input_file,  # Input file,
                 "-lavfi", "minterpolate=fps=20,scale=600:-2:flags=lanczos,setpts=1.3*PTS",
                 "-c:v", "gif",  # Use H.265 codec
-                output_file,         # Output file
+                output_file,  # Output file
                 "-y"
             ]
 
@@ -45,9 +50,14 @@ def extract_video_segments_as_gif(input_file, output_folder, segments):
 
 
 gifs = [
+    # START, END, LABEL
     (1, 3, "Ridicle"),
     (4, 6, "Threaten"),
-    (8, 9, "Applause")
+    (8, 9, "Applause"),
+    (10, 11, "LOL"),
+    (12, 13, "Hello"),
+    (14, 27, "CS-Ranked Heroic En"),
+    (27.5, 28, "Waiter Walk"),
 ]
 
 
